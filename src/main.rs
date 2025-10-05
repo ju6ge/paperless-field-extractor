a41ecdf77c55804d04cb71cb316f439871f9d3b8
ju6ge <judge@felixrichter.tech>
2025-10-05 18:20:52 +0200

    generate json schema from custom field definition

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,50 @@
-use paperless_api_client::{Client};
+use paperless_api_client::Client;
+use types::schema_from_custom_field;
 
+mod types;
+//mod extract;
 mod requests;
+
+async fn fetch_stuff_from_paperless(api_client: &mut Client) {
+    let c_fields = requests::get_all_custom_fields(api_client).await;
+    let tags = requests::get_all_tags(api_client).await;
+    let mut docs = requests::get_all_docs(api_client).await;
+
+    for cf in c_fields {
+        println!(
+            "{}",
+            serde_json::to_string_pretty(schema_from_custom_field(cf).unwrap().as_value()).unwrap()
+        )
+    }
+
+    docs = docs
+        .into_iter()
+        .filter(|d| {
+            let mut has_inbox_tag = false;
+            for t in &d.tags {
+                if let Some(tag_def) = tags
+                    .iter()
+                    .filter(|td| td.id == *t)
+                    .collect::<Vec<_>>()
+                    .first()
+                {
+                    has_inbox_tag = tag_def.is_inbox_tag.is_some_and(|v| v);
+                    break;
+                }
+            }
+            has_inbox_tag
+        })
+        .collect();
+
+    println!("{}", docs.len())
+}
 
 #[tokio::main]
 async fn main() {
     colog::init();
+
     let mut api_client = Client::new_from_env();
     api_client.set_base_url("https://judge-paperless.16.wlandt.de");
 
-    let _c_fields = requests::get_all_custom_fields(&mut api_client).await;
-    let tags = requests::get_all_tags(&mut api_client).await;
-    let mut docs = requests::get_all_docs(&mut api_client).await;
-
-    docs = docs.into_iter().filter(|d| {
-        let mut has_inbox_tag = false;
-        for t in &d.tags {
-            if let Some(tag_def) = tags.iter().filter(|td| td.id == *t).collect::<Vec<_>>().first() {
-                has_inbox_tag = tag_def.is_inbox_tag.is_some_and(|v| v);
-                break;
-            }
-        }
-        has_inbox_tag
-    }).collect();
-
-    println!("{}", docs.len())
+    fetch_stuff_from_paperless(&mut api_client).await;
 }
---
+++ src/types.rs
@@ -0,0 +1,81 @@
+use paperless_api_client::types::CustomField;
+use schemars::{JsonSchema, json_schema, schema_for};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+pub(crate) struct FieldExtract {
+    field_description: String,
+    field_value: Value,
+    alternative_values: Vec<Value>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct SelectOption {
+    id: String,
+    label: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct FieldSelect {
+    select_options: Vec<SelectOption>,
+}
+
+pub(crate) fn schema_from_custom_field(cf: CustomField) -> Option<schemars::Schema> {
+    let mut base_schema = schema_for!(FieldExtract);
+    // set field of description schema as a constant string value matching the name
+    // of the custom field. This should guide the llm token generation to extract the
+    // desired information from the document
+    base_schema.get_mut("properties").map(|properties| {
+        properties
+            .get_mut("field_description")
+            .map(|description_schema| {
+                *description_schema = json_schema!({ "const": cf.name }).as_value().clone()
+            });
+        properties
+    });
+    let field_schema = match cf.data_type {
+        paperless_api_client::types::DataTypeEnum::String => schema_for!(String),
+        paperless_api_client::types::DataTypeEnum::Date => schema_for!(chrono::NaiveDate),
+        paperless_api_client::types::DataTypeEnum::Boolean => schema_for!(bool),
+        paperless_api_client::types::DataTypeEnum::Integer => schema_for!(i64),
+        paperless_api_client::types::DataTypeEnum::Float => schema_for!(f64),
+        paperless_api_client::types::DataTypeEnum::Monetary => schema_for!(f64),
+        paperless_api_client::types::DataTypeEnum::Select => {
+            let select_options: FieldSelect = if let Some(v) = &cf.extra_data {
+                serde_json::from_value(v.clone()).unwrap()
+            } else {
+                return None;
+            };
+            let enum_values = serde_json::to_string(
+                &select_options
+                    .select_options
+                    .into_iter()
+                    .map(|o| o.label)
+                    .collect::<Vec<_>>(),
+            )
+            .unwrap();
+            json_schema!({
+                "type": "string",
+                "enum": enum_values
+            })
+        }
+        paperless_api_client::types::DataTypeEnum::Url
+        | paperless_api_client::types::DataTypeEnum::Documentlink => {
+            return None;
+        }
+    };
+    // set the schema of the field value according to the type of custom field
+    base_schema.get_mut("properties").map(|properties| {
+        properties
+            .get_mut("field_value")
+            .map(|value_schema| *value_schema = field_schema.as_value().clone());
+        properties.get_mut("alternative_values").map(|array| {
+            array
+                .get_mut("items")
+                .map(|value_schema| *value_schema = field_schema.as_value().clone());
+        });
+        properties
+    });
+    Some(base_schema)
+}
