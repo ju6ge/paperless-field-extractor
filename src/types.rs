40937f07022cc3463b8740028eac9dbc2d93c51f
ju6ge <judge@felixrichter.tech>
2025-10-11 23:35:58 +0200

    implement logic to convert extracted custom field to an custom field instance for upload to paperless

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,5 @@
-use paperless_api_client::types::CustomField;
+use chrono::NaiveDate;
+use paperless_api_client::types::{CustomField, CustomFieldInstance, DataTypeEnum};
 use schemars::{JsonSchema, json_schema, schema_for};
 use serde::{Deserialize, Serialize};
 use serde_json::{Value, json};
@@ -29,6 +30,112 @@ pub(crate) struct FieldExtract {
     /// custom field suggestions would need to be implemented for paperless first. I don't think they are
     /// at the moment
     alternative_values: Vec<Value>,
+}
+
+#[derive(Debug, Error)]
+pub(crate) enum FieldError {
+    #[error("custom fields with type {0} are not supported!")]
+    UnsupportedCustomFieldType(DataTypeEnum),
+    #[error("could not parse custom field value to the expected custom field type")]
+    ParsingError(#[from] serde_json::error::Error),
+    #[error("custom field `{0}` is of type enum but has no variants defined!")]
+    EmptyEnumValuesForCustomField(String),
+    #[error("matching {0} variant does not uniquely match any variant of custom field {1}")]
+    NoUniqueEnumValueFound(String, String),
+}
+
+impl FieldExtract {
+    pub fn to_custom_field_instance(
+        &self,
+        custom_field_spec: &CustomField,
+    ) -> Result<CustomFieldInstance, FieldError> {
+        // try to parse the value into the specified type and transform
+        // it to the representation required for paperless to set
+        // custom field based via the API
+        match custom_field_spec.data_type {
+            paperless_api_client::types::DataTypeEnum::Boolean => {
+                let _parsed_value: bool = serde_json::from_value(self.field_value.clone())?;
+                Ok(CustomFieldInstance {
+                    value: Some(self.field_value.clone()),
+                    field: custom_field_spec.id,
+                })
+            }
+            paperless_api_client::types::DataTypeEnum::String => {
+                let _parsed_value: String = serde_json::from_value(self.field_value.clone())?;
+                Ok(CustomFieldInstance {
+                    value: Some(self.field_value.clone()),
+                    field: custom_field_spec.id,
+                })
+            }
+            paperless_api_client::types::DataTypeEnum::Date => {
+                let _parsed_value: NaiveDate = serde_json::from_value(self.field_value.clone())?;
+                Ok(CustomFieldInstance {
+                    value: Some(self.field_value.clone()),
+                    field: custom_field_spec.id,
+                })
+            }
+            paperless_api_client::types::DataTypeEnum::Integer => {
+                let _parsed_value: i64 = serde_json::from_value(self.field_value.clone())?;
+                Ok(CustomFieldInstance {
+                    value: Some(self.field_value.clone()),
+                    field: custom_field_spec.id,
+                })
+            }
+            paperless_api_client::types::DataTypeEnum::Float => {
+                let _parsed_value: f64 = serde_json::from_value(self.field_value.clone())?;
+                Ok(CustomFieldInstance {
+                    value: Some(self.field_value.clone()),
+                    field: custom_field_spec.id,
+                })
+            }
+            paperless_api_client::types::DataTypeEnum::Monetary => {
+                let parsed_currency_value: CurrencyValue =
+                    serde_json::from_value(self.field_value.clone())?;
+                Ok(CustomFieldInstance {
+                    value: Some(Value::String(format!(
+                        "{}{}",
+                        parsed_currency_value.currency_code, parsed_currency_value.value
+                    ))),
+                    field: custom_field_spec.id,
+                })
+            }
+            paperless_api_client::types::DataTypeEnum::Select => {
+                let enum_variant: String = serde_json::from_value(self.field_value.clone())?;
+                let select_options: FieldSelect = if let Some(v) = &custom_field_spec.extra_data {
+                    serde_json::from_value(v.clone()).unwrap()
+                } else {
+                    return Err(FieldError::EmptyEnumValuesForCustomField(
+                        custom_field_spec.name.clone(),
+                    ));
+                };
+                let matching_enum_values = &select_options
+                    .select_options
+                    .into_iter()
+                    .filter(|cfi| *cfi.label == enum_variant)
+                    .collect::<Vec<_>>();
+                if matching_enum_values.len() != 1 {
+                    Err(FieldError::NoUniqueEnumValueFound(
+                        enum_variant,
+                        custom_field_spec.name.clone(),
+                    ))
+                } else {
+                    let matching_enum_variant = matching_enum_values
+                        .first()
+                        .expect("if case checks that exactly one element exists");
+                    Ok(CustomFieldInstance {
+                        // the value of a custom field corresponds to the string id of the the enum variant field
+                        value: Some(serde_json::Value::String(matching_enum_variant.id.clone())),
+                        field: custom_field_spec.id,
+                    })
+                }
+            }
+            paperless_api_client::types::DataTypeEnum::Url
+            | paperless_api_client::types::DataTypeEnum::Documentlink => Err(
+                FieldError::UnsupportedCustomFieldType(custom_field_spec.data_type.clone()),
+            ),
+        }
+        //TODO figure out what to do with alternative value suggestions
+    }
 }
 
 #[derive(Serialize, Deserialize)]
