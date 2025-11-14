7c331ab605be17dc21a6cc05e26b2d293f344265
ju6ge <judge@felixrichter.tech>
2025-11-14 02:21:02 +0100

    implement receiving webhooks

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,30 @@
+use std::sync::Arc;
+
 use actix_web::{
-    dev::{HttpServiceFactory, Url}, error::PayloadError, get, http::{uri, Uri}, post, web::{self, Data}, App, HttpResponse, HttpResponseBuilder, HttpServer
+    App, HttpResponse, HttpResponseBuilder, HttpServer, ResponseError,
+    dev::{HttpServiceFactory, Url},
+    error::PayloadError,
+    get,
+    http::{StatusCode, Uri, uri},
+    post,
+    web::{self, Data},
 };
-use paperless_api_client::{Client, types::Tag};
+use once_cell::sync::Lazy;
+use paperless_api_client::{
+    Client,
+    types::{Document, Tag},
+};
+use regex::Regex;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 use tokio::{join, spawn, task::spawn_blocking};
 
-use crate::config::{self, Config};
+static DOCID_REGEX: Lazy<Regex> = regex_static::lazy_regex!(r"documents/(\d*)");
+
+use crate::{
+    config::{self, Config},
+    requests,
+};
 
 #[non_exhaustive]
 enum ProcessingType {
@@ -17,31 +36,100 @@ enum ProcessingType {
 /// Since LLM are notoriously resource intensive a task queue is used in order to facilitate asyncronous
 /// batched processinsg a container is required to hold the queue of processing requests
 struct DocumentProcessingRequest {
-    document_id: i64,
+    document: Document,
     processing_type: ProcessingType,
 }
 
-#[get("/fill/custom_fields")]
+#[post("/fill/custom_fields")]
 async fn custom_field_prediction(
     params: web::Json<WebhookParams>,
     status_tags: Data<PaperlessStatusTags>,
     api_client: Data<Client>,
     config: Data<Config>,
     document_pipeline: web::Data<tokio::sync::mpsc::UnboundedSender<DocumentProcessingRequest>>,
-) -> HttpResponse {
-    let doc_url: Uri = params.document_url.parse().unwrap();
-    let configured_paperless_server: Uri = config.paperless_server.parse().unwrap();
-    // verify document host and configured paperless instance are the same host to avoid handling requests from other paperless instances
-    if doc_url.host().is_some_and(|rs| configured_paperless_server.host().is_some_and(|cs| cs == rs)) {
-        HttpResponse::Accepted().into()
-    } else {
-        HttpResponse::Unauthorized().into()
-    }
+) -> Result<HttpResponse, WebhookError> {
+    let _ = params
+        .handle_request(
+            status_tags,
+            api_client,
+            config,
+            document_pipeline,
+            ProcessingType::CustomFieldPrediction,
+        )
+        .await?;
+    Ok(HttpResponse::Accepted().into())
+}
+
+#[derive(Debug, thiserror::Error)]
+enum WebhookError {
+    #[error("Document with id `{0}` does not exist!")]
+    DocumentDoesNotExist(i64),
+    #[error("Document ID is not a valid integer!")]
+    InvalidDocumentId,
+    #[error("Could not parse Document ID from `document_url` field!")]
+    DocumentUrlParsingIDFailed,
+    #[error("Document Url points to a server unrelated to this configuration. Ignoring Request")]
+    ReceivedRequestFromUnconfiguredServer,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct WebhookParams {
-    document_url: String
+    document_url: String,
+}
+
+impl ResponseError for WebhookError {
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match self {
+            WebhookError::ReceivedRequestFromUnconfiguredServer => StatusCode::UNAUTHORIZED,
+            _ => StatusCode::BAD_REQUEST,
+        }
+    }
+}
+
+impl WebhookParams {
+    async fn handle_request(
+        &self,
+        status_tags: Data<PaperlessStatusTags>,
+        api_client: Data<Client>,
+        config: Data<Config>,
+        document_pipeline: web::Data<tokio::sync::mpsc::UnboundedSender<DocumentProcessingRequest>>,
+        req_type: ProcessingType,
+    ) -> Result<(), WebhookError> {
+        let doc_url: Uri = self.document_url.parse().unwrap();
+        let configured_paperless_server: Uri = config.paperless_server.parse().unwrap();
+        // verify document host and configured paperless instance are the same host to avoid handling requests from other paperless instances
+        if doc_url.host().is_some_and(|rs| {
+            configured_paperless_server
+                .host()
+                .is_some_and(|cs| cs == rs)
+        }) {
+            if let Some(doc_id_cap) = DOCID_REGEX.captures(doc_url.path()) {
+                if let Some(doc_id) = doc_id_cap
+                    .get(1)
+                    .and_then(|v| v.as_str().parse::<i64>().ok())
+                {
+                    let mut api_client =
+                        Arc::<Client>::make_mut(&mut api_client.into_inner()).clone();
+                    if let Ok(doc) = api_client.documents().retrieve(doc_id, None, None).await {
+                        //TODO update document tags
+                        let _ = document_pipeline.send(DocumentProcessingRequest {
+                            document: doc,
+                            processing_type: req_type,
+                        });
+                        Ok(())
+                    } else {
+                        Err(WebhookError::DocumentDoesNotExist(doc_id))
+                    }
+                } else {
+                    Err(WebhookError::InvalidDocumentId)
+                }
+            } else {
+                Err(WebhookError::DocumentUrlParsingIDFailed)
+            }
+        } else {
+            Err(WebhookError::ReceivedRequestFromUnconfiguredServer)
+        }
+    }
 }
 
 #[post("/suggest/correspondent")]
@@ -51,15 +139,17 @@ async fn suggest_correspondent(
     api_client: Data<Client>,
     config: Data<Config>,
     document_pipeline: web::Data<tokio::sync::mpsc::UnboundedSender<DocumentProcessingRequest>>,
-) -> HttpResponse {
-    let doc_url: Uri = params.document_url.parse().unwrap();
-    let configured_paperless_server: Uri = config.paperless_server.parse().unwrap();
-    // verify document host and configured paperless instance are the same host to avoid handling requests from other paperless instances
-    if doc_url.host().is_some_and(|rs| configured_paperless_server.host().is_some_and(|cs| cs == rs)) {
-        HttpResponse::Accepted().into()
-    } else {
-        HttpResponse::Unauthorized().into()
-    }
+) -> Result<HttpResponse, WebhookError> {
+    let _ = params
+        .handle_request(
+            status_tags,
+            api_client,
+            config,
+            document_pipeline,
+            ProcessingType::CorrespondentSuggest,
+        )
+        .await?;
+    Ok(HttpResponse::Accepted().into())
 }
 
 struct DocumentProcessingApi;
@@ -73,10 +163,10 @@ impl HttpServiceFactory for DocumentProcessingApi {
 
 async fn document_processor(
     mut processing_request_channel: tokio::sync::mpsc::UnboundedReceiver<DocumentProcessingRequest>,
-    status_tags: PaperlessStatusTags
+    status_tags: PaperlessStatusTags,
 ) {
     while let Some(prc_req) = processing_request_channel.recv().await {
-        println!("Received Request for Document {}", prc_req.document_id);
+        println!("Received Request for Document {:#?}", prc_req.document);
     }
 }
 
